49b57fa7be78389d3dfe5b64c49855740c08255b
Filipe Belatti <fibelatti@outlook.com>
2015-06-01 23:54:57 -0300

    Implemented Spotify Search API

--- js/util.js
+++ js/util.js
@@ -38,56 +38,49 @@ function showDayEventsPopover(ref, events) {
 }
 
 $(".bands-ajax").select2({
-  placeholder: 'Procure por bandas',
   width: '100%',
   ajax: {
-    url: "https://api.spotify.com/v1/search",
-    dataType: 'json',
+    url: "https://api.spotify.com/v1/search?type=artist",
     delay: 250,
     data: function (params) {
       return {
-        q: params.term, // search term
-        page: params.page,
-        type: 'artist'
+        q: params.term
       };
     },
     processResults: function (data, page) {
-      // parse the results into the format expected by Select2.
-      // since we are using custom formatting functions we do not need to
-      // alter the remote JSON data
       return {
-        results: data.items
+        results: data.artists.items
       };
     },
     cache: true
   },
-  escapeMarkup: function (markup) { return markup; }, // let our custom formatter work
-  minimumInputLength: 1
+  escapeMarkup: function (markup) { return markup; },
+  minimumInputLength: 2,
+  templateResult: formatArtist, // omitted for brevity, see the source of this page
+  templateSelection: formatArtistSelection // omitted for brevity, see the source of this page
 });
 
-function formatRepo (repo) {
-  if (repo.loading) return repo.text;
+function formatArtist (artist) {
+  if (artist.loading) return artist.text;
+  
+  console.log(artist);
 
-  var markup = '<div class="clearfix">' +
-  '<div class="col-sm-1">' +
-  '<img src="' + repo.owner.avatar_url + '" style="max-width: 100%" />' +
-  '</div>' +
+  var markup = '<div class="clearfix result-row">' +
+  '<div class="col-sm-1">'
+  
+  if (artist.images[3] !== undefined) markup += '<img src="' + artist.images[3].url + '" style="width: 50px;height: 50px;" />'
+    
+  markup += '</div>' +
   '<div clas="col-sm-10">' +
   '<div class="clearfix">' +
-  '<div class="col-sm-6">' + repo.full_name + '</div>' +
-  '<div class="col-sm-3"><i class="fa fa-code-fork"></i> ' + repo.forks_count + '</div>' +
-  '<div class="col-sm-2"><i class="fa fa-star"></i> ' + repo.stargazers_count + '</div>' +
+  '<div class="col-sm-6">' + artist.name + '</div>'
   '</div>';
-
-  if (repo.description) {
-    markup += '<div>' + repo.description + '</div>';
-  }
 
   markup += '</div></div>';
 
   return markup;
 }
 
-function formatRepoSelection (repo) {
-  return repo.full_name || repo.text;
+function formatArtistSelection (artist) {
+  return artist.name || artist.text;
 }
